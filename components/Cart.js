d3c65450216a05ba41b7b5dea7f9a141cd8c1137
SavaniShubham <shubham.savani2004@gmail.com>
2024-05-21 10:55:36 +0530

    add some feature in cart (pricing , remove etc.)

--- components/Cart.js
+++ components/Cart.js
@@ -1,28 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { CON_URL } from "../utils/constants";
-import { clearitem, removeitem } from "../utils/cartslice";
+import { additem, clearitem, removeitem } from "../utils/cartslice";
+import Shimmer from "./Shimmer";
 
 const Cart = ()=>
     {
         const cartitem = useSelector((store)=> store.cart.items);
         console.log(cartitem);
+        const totalprice = useSelector((store) => store.cart.totalprice);
+        console.log(totalprice);
+        let Gst = Math.floor((totalprice*11)/100);
         const dispatch =useDispatch();
-        // const handleremoveclick = (item)=>
-        //   {
-        //       dispatch(removeitem(item.card.info.id));
-        //   }
-
+        const handleremoveclick = (item)=>
+          {
+              dispatch(removeitem(item));
+          }
           const clearcart = ()=>
             {
               dispatch(clearitem());
             }
       
-        return(
+        return( totalprice == 0 ? <Shimmer/> :
             <div className=" w-3/4 m-auto flex justify-between">
-                     <div>
-                        
-                      
-                         <div className=" absolute mx-[700px] mt-5 w-14 h-10 text-2xl bg-white text-gray-400 text-center  shadow-lg">
+                     <div >
+                         <div className=" absolute mx-[800px] mt-5 w-14 h-10 text-2xl bg-white text-gray-400 text-center  shadow-lg">
                          <button onClick={()=> clearcart()} >x</button>
                          </div>
                          {
@@ -32,7 +33,7 @@ const Cart = ()=>
 
                                  
                                    <div>
-                                    <img className=" w-20 h-24 pb-1 rounded-xl cursor-pointer" src={CON_URL+item.card.info.imageId}></img>
+                                    <img className=" w-20 h-20pb-1 rounded-xl cursor-pointer" src={CON_URL+item.card.info.imageId}></img>
                                    </div>
                                   
                                   <div className="   items-center font-mono  ">
@@ -40,11 +41,13 @@ const Cart = ()=>
                                       <span>{item.card.info.itemAttribute.vegClassifier ==="VEG" ? "🟩" : "🟥"}</span>
                                        <div className=" flex ">
                                          <div className=" w-96 font-bold text-gray-950">{item.card.info.name}</div>
-                                        <div className=" ml-24 font-semibold tracking-tight">Rs {item.card.info.defaultPrice/100 || item.card.info.finalPrice/100  || item.card.info.price/100 }</div>
+                                        <div className=" ml-56 font-semibold tracking-tight">Rs {item.card.info.defaultPrice/100 || item.card.info.finalPrice/100  || item.card.info.price/100 }</div>
                                       </div>
                                        
                                             <div className= " mt-3 w-36 shadow-xl rounded-lg">
-                                          <button className=" w-7 border ">-</button><button className=" w-20">Add</button><button className="w-7 border">+</button>
+                                          <button className=" w-7 border " onClick={()=>handleremoveclick(item)}>-</button>
+                                          <button className=" w-20">Add</button>
+                                          <button className="w-7 border" onClick={()=>handleaddclick(item)}>+</button>
                                          </div>
                         
                                      </div>
@@ -56,13 +59,25 @@ const Cart = ()=>
                         }
                       </div>
 
-                      <div className=" bg-slate-300 mt-8">
+                      <div className="  bg-gray-200 mt-32 w-80 h-44 rounded-lg shadow-lg">
                        
-                             <div>Bil Details</div>
-                             <div>item total</div>
-                             <div>platform fee</div>
-                             <div>GST and Restaurant charges</div>
-                             <div>To pay</div>
+                             <div className=" ml-3 mt-4 font-semibold text-lg">Bil Details</div>
+                             <div className="flex justify-between">
+                               <div className=" ml-3 leading-7">
+                                <div>item total</div>
+                                <div>platform fee</div>
+                                <div>GST </div>
+                                <hr></hr><hr></hr>
+                                <div className=" font-bold">To pay</div>
+                             </div>
+                             <div className="mr-4 leading-7">
+                             <div > ₹{totalprice} </div>
+                             <div > ₹ 5 </div>
+                             <div > ₹{Gst} </div>
+                             <hr></hr><hr></hr><hr></hr><hr></hr>
+                             <div  className=" font-bold"> ₹{totalprice + 5 + Gst} </div>
+                             </div>
+                             </div>
                        
                       </div>
             </div>
--- utils/cartslice.js
+++ utils/cartslice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 
 const cartslice = createSlice(
@@ -6,19 +6,27 @@ const cartslice = createSlice(
         name:"cart",
         initialState:{
             items:[],
+            totalprice:0,
         },
+     
         reducers:{
             additem: (state , action)=>
                 {
                     state.items.push(action.payload);
+                    state.totalprice = state.totalprice + (action.payload.card.info.defaultprice/100 ||action.payload.card.info.finalprice/100 ||action.payload.card.info.price/100) ;
+
+                   
                 },
             removeitem: (state , action)=>
                     {
-                       state.items.pop();
+                      id=action.payload.card.info.id ;
+                      state.items= state.items.filter(item => item.card.info.id !=id );
+                      state.totalprice = state.totalprice - (action.payload.card.info.defaultprice/100 ||action.payload.card.info.finalprice/100 ||action.payload.card.info.price/100) ;
                     },
             clearitem :(state)=>
                 {
                     state.items.length = 0;
+                    state.totalprice = 0;
                 },
         },
     }

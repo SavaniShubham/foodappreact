63fc43f5d535233f4407991b4cff1cc7f7b84349
SavaniShubham <shubham.savani2004@gmail.com>
2024-05-21 16:38:25 +0530

    setting up testing in our app

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  };
+
+  //types of testing(developer)
+  //unit testing
+  //integration testing
+  //end to end testing
+
+  //setting up testing in our app
+  //install react testing library
+  //install jest
+  //install babel dependencies
+  //configure babel(babel.config.js)
+  //configure parcel configure file to disable default transplition(.parcelrc)
